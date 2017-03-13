5edafa8ddea6be46564e67ed95a7d94008a26774
Alex Chistyakov <alexclear@gmail.com>
2017-03-13 07:03:21 +0300

    Use 1.3.14

--- templates/deluge-web.sh
+++ templates/deluge-web.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /opt/deluge/deluge-1.3.6
+cd /opt/deluge/deluge-1.3.14
 source env/bin/activate
 deluge-web -c /var/lib/deluge/.config/deluge &
 echo $! > /var/run/deluge-web-eye.pid
--- templates/deluge.rb
+++ templates/deluge.rb
@@ -1,5 +1,5 @@
 Eye.application 'deluge' do
-  working_dir '/opt/deluge/deluge-1.3.6'
+  working_dir '/opt/deluge/deluge-1.3.14'
   stdall '/var/log/eye/deluge-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
